4cd7060fe5fc6d0393e99284e8ff8df1991e5a2c
PayOT <payot.developer@gmail.com>
2025-07-03 13:31:18 +0900

    trpc endpoint edit

--- app/stores/user.store.ts
+++ app/stores/user.store.ts
@@ -2,4 +2,4 @@
 export const useUserStore = defineStore('user', () => {
   const id = ref(1);
   return { id }
-}, { persist: { storage: sessionStorage } })
+}, { persist: { storage: sessionStorage } });
