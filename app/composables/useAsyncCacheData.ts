95be330ecba4254a1654e4011bd40f68fb6bcd59
PayOT <payot.developer@gmail.com>
2025-07-07 16:43:30 +0900

    useAsyncCacheData 요청 조건부 수정

--- app/composables/useAsyncCacheData.ts
+++ app/composables/useAsyncCacheData.ts
@@ -14,6 +14,7 @@ export function useAsyncCacheData<T>(key: MaybeRefOrGetter<string>, fetcher: () 
     getCachedData(key, app) {
       return app.payload.data[key];
     },
+    dedupe: 'defer',
     ...options,
   })
 }
